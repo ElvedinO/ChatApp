5d4fb75101a4f2bf2a32a7087f3da9cddd0a7141
Elvedin <vedo_o.95@hotmail.com>
2024-10-29 21:31:30 +0100

    media fix

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { auth, db } from '../../lib/firebase';
 import { useChatStore } from '../../lib/chatStore';
 import { arrayRemove, arrayUnion, doc, updateDoc } from 'firebase/firestore';
@@ -14,6 +14,9 @@ const Detail = () => {
     resetChat,
   } = useChatStore();
   const { currentUser } = useUserStore();
+
+  const [isMediaVisible, setIsMediaVisible] = useState(true);
+  const [isArrowRotated, setIsArrowRotated] = useState(false);
 
   const handleBlock = async () => {
     if (!user) return;
@@ -34,6 +37,12 @@ const Detail = () => {
     auth.signOut();
     resetChat();
   };
+
+  const toggleMediaVisibility = () => {
+    setIsMediaVisible(!isMediaVisible);
+    setIsArrowRotated(!isArrowRotated);
+  };
+
   return (
     <div className='flex-[1] overflow-scroll no-scrollbar'>
       <div className='user py-7 px-5 flex flex-col items-center gap-4 border-b border-bordergray text-center'>
@@ -44,46 +53,56 @@ const Detail = () => {
         />
         <h2>{user?.username}</h2>
       </div>
-      <div className='info p-5 flex flex-col gap-6  '>
-        <div className='option '>
-          <div className='title'>
+      <div className='info p-5 flex flex-col gap-6'>
+        <div className='option'>
+          <div className='title' onClick={toggleMediaVisibility}>
             <span>Media</span>
-            <img className='arrowIcons' src='../images/arrowDown.png' alt='' />
+            <img
+              className={`arrowIcons ${isArrowRotated ? 'rotate-180' : ''}`}
+              src='../images/arrowDown.png'
+              alt=''
+              style={{
+                transform: isArrowRotated ? 'rotate(180deg)' : 'rotate(0deg)',
+                transition: 'transform 0.3s',
+              }}
+            />
           </div>
-          <div className='photos grid grid-cols-2 gap-5 mt-5'>
-            <div className='photoItem'>
-              <div className='photoDetail'>
-                <img
-                  src='https://images.pexels.com/photos/28927948/pexels-photo-28927948/free-photo-of-dramatic-canyon-landscape-on-remote-island.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-                  alt=''
-                />
+          {isMediaVisible && (
+            <div className='photos grid grid-cols-2 gap-5 mt-5'>
+              <div className='photoItem'>
+                <div className='photoDetail'>
+                  <img
+                    src='https://images.pexels.com/photos/28927948/pexels-photo-28927948/free-photo-of-dramatic-canyon-landscape-on-remote-island.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+                    alt=''
+                  />
+                </div>
+              </div>
+              <div className='photoItem'>
+                <div className='photoDetail'>
+                  <img
+                    src='https://images.pexels.com/photos/28927948/pexels-photo-28927948/free-photo-of-dramatic-canyon-landscape-on-remote-island.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+                    alt=''
+                  />
+                </div>
+              </div>
+              <div className='photoItem'>
+                <div className='photoDetail'>
+                  <img
+                    src='https://images.pexels.com/photos/28927948/pexels-photo-28927948/free-photo-of-dramatic-canyon-landscape-on-remote-island.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+                    alt=''
+                  />
+                </div>
+              </div>
+              <div className='photoItem'>
+                <div className='photoDetail'>
+                  <img
+                    src='https://images.pexels.com/photos/28927948/pexels-photo-28927948/free-photo-of-dramatic-canyon-landscape-on-remote-island.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+                    alt=''
+                  />
+                </div>
               </div>
             </div>
-            <div className='photoItem'>
-              <div className='photoDetail'>
-                <img
-                  src='https://images.pexels.com/photos/28927948/pexels-photo-28927948/free-photo-of-dramatic-canyon-landscape-on-remote-island.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-                  alt=''
-                />
-              </div>
-            </div>
-            <div className='photoItem'>
-              <div className='photoDetail'>
-                <img
-                  src='https://images.pexels.com/photos/28927948/pexels-photo-28927948/free-photo-of-dramatic-canyon-landscape-on-remote-island.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-                  alt=''
-                />
-              </div>
-            </div>
-            <div className='photoItem'>
-              <div className='photoDetail'>
-                <img
-                  src='https://images.pexels.com/photos/28927948/pexels-photo-28927948/free-photo-of-dramatic-canyon-landscape-on-remote-island.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-                  alt=''
-                />
-              </div>
-            </div>
-          </div>
+          )}
         </div>
       </div>
       <div className='flex flex-col p-5 gap-5 border-t border-bordergray'>
